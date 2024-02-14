e4a3105166da00afd2ca2fc0680a54f70400df42
Jake <jakerson.dev@gmail.com>
2024-02-14 23:21:59 +0800

    setup the express application into index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+//import express module
+const express = require("express");
+//initialized express module
+const app = express();
+
+//use the built-in function/middleware of express for json an body-parser
+app.use(express.json());
+app.use(express.urlencoded());
+
+//using Port 5000
+const PORT = 5000;
+
+
+
+//create listen for express
+app.listen(PORT, () =>{
+    console.info(`
+    Port: ${PORT}
+    Env: Development
+    Application: http://localhost:${PORT}
+    `);
+});
