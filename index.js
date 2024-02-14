ff52ec58b82697cb927f52ed7c72fc8f54774c48
Jake <jakerson.dev@gmail.com>
2024-02-15 00:05:32 +0800

    feat: add new get GET function to gell all the student record, add new col for isActive into User table

--- index.js
+++ index.js
@@ -10,17 +10,32 @@ app.use(express.urlencoded());
 //using Port 5000
 const PORT = 5000;
 
+
+//GET - fetch all student record 
+app.get('/student', (_, response) => {
+    //query for example to get all the student data
+    const data = {};//select * from Student
+  //send response and the data
+    response.status(200).json(
+        {
+            success: true,
+            status: 200,
+            data: data
+        }
+    );
+});
+
 //POST - to add new student record
 app.post('/student/addStudent', (request, response) => {
     //show the request body
     console.log(request.body);
     //Destructuring studentId , firstName, and lastName
-    const {studentId,firstName,lastName} = request.body
+    const { studentId, firstName, lastName } = request.body
     //send response if success and send message and status (200)
     response.status(201).json({
         success: true,
         status: 201,
-        message: `Student: ${studentId} - ${firstName + ' ' +lastName} is sucessfully added.`
+        message: `Student: ${studentId} - ${firstName + ' ' + lastName} is sucessfully added.`
 
     });
 });
